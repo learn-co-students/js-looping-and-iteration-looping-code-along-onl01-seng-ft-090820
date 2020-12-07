1e8259755a1a70df8ca165a256327d209a5b208d
LeahESc <leahemily1@gmail.com>
2020-12-07 11:20:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, occasion) { 
+    const new_array = [] 
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return new_array;
+}
+
+function countDown(int) { 
+    while (int > -1 ) { 
+        console.log(int);
+        --int;
+    }
+}
