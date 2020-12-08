cd59ca8e4cc3525f11d9594ceec6c6269b641291
doosemavis <moosedavis2011@gmail.com>
2020-12-08 15:53:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(thankYou, eventName) {
+    const messages = []
+    for (let i = 0; i < thankYou.length; i++) {
+        messages.push(`Thank you, ${thankYou[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber);
+}
