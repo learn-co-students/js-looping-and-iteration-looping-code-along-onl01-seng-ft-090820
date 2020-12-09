af2f96b457d0a6c80bc1fc4c5764f663431f6e91
brockbyrd <brockbyrdd@gmail.com>
2020-12-09 10:57:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, string) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+
+    return newArray;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
