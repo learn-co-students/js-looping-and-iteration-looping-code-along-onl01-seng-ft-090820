ce4801735fd5d6e43ea8f2b65800ba0753b0c9c9
Valeria Copleman <valeriacopleman@gmail.com>
2020-12-14 06:48:28 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const gifts = ["Lisa", "Kaitlin", "Jan"]
+ 
+function writeCards(gifts, event) {
+    let messages=[]
+    for (let i = 0; i < gifts.length; i++) {
+        messages.push(`Thank you, ${gifts[i]}, for the wonderful ${event} gift!`);
+    }
+ 
+    return messages;
+}
+ 
+writeCards(gifts, 'surprise');
+
+
+function countDown(num) {
+    while (num != -1) {
+        console.log(num--)
+      }
+}
