ec9c99b44a591226c9093f4a30fb1924aeb87720
adarvin14 <amdarvin14@gmail.com>
2020-12-14 12:30:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(cards, occasion) { 
+    const new_array = []
+    for (let i = 0; i < cards.length; i++) { 
+    new_array.push(`Thank you, ${cards[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return new_array;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
