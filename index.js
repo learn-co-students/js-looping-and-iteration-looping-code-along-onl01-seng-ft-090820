8d4969da366c0ff5268a826cfe961f580c304fa9
TravisSLee <travisslee223@gmail.com>
2020-12-16 14:27:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Lisa", "Kaitlin", "Jan"];
+const event = "surpise"
+function writeCards(cards, event) {
+    let array = []
+    for (let i = 0; i < cards.length; i++) {
+        array.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+      }
+      return array;
+};
+
+let number = 10
+
+function countDown(number) {
+    while (number >= 0){
+        console.log(number);
+        number -= 1;
+    }
+};
